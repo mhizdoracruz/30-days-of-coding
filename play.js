872b4da883f0185c22b66dce5627039539f446ae
DOMINIC DORATHY <dorathydominic3@gmail.com>
2021-08-20 17:52:41 -0700

    first commit

---
+++ play.js
@@ -0,0 +1,12 @@
+alert ('Hello World');
+
+function bioData() {
+    let name = 'Dorathy';
+    let nickName = 'Mhizdoracruz';
+    let age = '19 years old';
+    let stateOforigin = 'Rivers state';
+    let gender = 'Female';
+    let nationality = 'Nigerian';
+    console.log = ([name, nickname, age, stateOfOrigin, gender, nationality]);
+}
+bioData();
